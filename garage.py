db240caf798d6289d43341c551b8bfb8824c738d
RealRottale1 <camarkham2@gmail.com>
2026-04-23 07:50:59 -0400

    ea

--- garage.py
+++ garage.py
@@ -1,5 +1,7 @@
 def enter_garage(garage, car_id, entry_hour):
     if len(garage["cars"]) >= garage["capacity"]: 
         raise ValueError
+    if (not type(car_id) is int):
+        raise ValueError
     garage["cars"].append(car_id)
     return True
