9f8c5c90ecfa2798fe3f18209de8443e4f253c14
RealRottale1 <camarkham2@gmail.com>
2026-04-23 07:49:26 -0400

    e

--- test_garage.py
+++ test_garage.py
@@ -1,5 +1,17 @@
 import pytest
 from garage import enter_garage
 
-def test_entergarage():
-    assert enter_garage() == True
+def test_entergaragePass():
+    garage = {
+        "capacity": 10,
+        "cars": {}
+    }
+    assert enter_garage(garage, 0, 2) == True
+
+def test_enterFullGarage():
+    garage = {
+        "capacity": 0,
+        "cars": {}
+    }
+    with pytest.raises(ValueError):
+        enter_garage(garage, 0, 2)
